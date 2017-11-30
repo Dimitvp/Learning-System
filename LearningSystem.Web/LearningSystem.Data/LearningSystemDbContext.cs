c45571f4ff1b936028f882aa2dbcc2e8fd12e3dd
Dimitar Nikolov <zeitgeistdimi@gmail.com>
2017-11-30 14:27:26 +0200

    Creating the project skeleton

---
+++ LearningSystem.Web/LearningSystem.Data/LearningSystemDbContext.cs
@@ -0,0 +1,20 @@
+﻿namespace LearningSystem.Data
+{
+    using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+    using Microsoft.EntityFrameworkCore;
+    using Models;
+
+    public class LearningSystemDbContext : IdentityDbContext<User>
+    {
+        public LearningSystemDbContext(DbContextOptions<LearningSystemDbContext> options)
+            : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+            
+        }
+    }
+}
