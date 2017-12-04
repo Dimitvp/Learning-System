ba774d33b2c86245e65bdfe5e4439406d622d70b
Dimitar Nikolov <zeitgeistdimi@gmail.com>
2017-12-04 14:17:01 +0200

    Adding courses functionality with assigning trainer.

--- LearningSystem.Web/LearningSystem.Web/Areas/Admin/Controllers/CoursesController.cs
+++ LearningSystem.Web/LearningSystem.Web/Areas/Admin/Controllers/CoursesController.cs
@@ -1,9 +1,72 @@
 ﻿namespace LearningSystem.Web.Areas.Admin.Controllers
 {
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Threading.Tasks;
+    using Data.Models;
+    using LearningSystem.Services.Admin;
+    using LearningSystem.Web.Controllers;
+    using LearningSystem.Web.Infrastructure.Extensions;
+    using Models.Courses;
+    using Microsoft.AspNetCore.Identity;
     using Microsoft.AspNetCore.Mvc;
+    using Microsoft.AspNetCore.Mvc.Rendering;
 
     public class CoursesController : BaseAdminController
     {
-        public IActionResult Creat() => View();
+        private readonly UserManager<User> userManager;
+        private readonly IAdminCourseService courses;
+
+        public CoursesController(UserManager<User> userManager, IAdminCourseService courses)
+        {
+            this.userManager = userManager;
+            this.courses = courses;
+        }
+
+        public async Task<IActionResult> Create()
+            => View(new AddCourseFormModel
+            {
+                Trainers = await this.GetTrainers(),
+                StartDate = DateTime.UtcNow,
+                EndDate = DateTime.UtcNow.AddDays(30)
+            });
+
+        [HttpPost]
+        public async Task<IActionResult> Create(AddCourseFormModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                model.Trainers = await this.GetTrainers();
+                return this.View(model);
+            }
+
+            await this.courses.Create(
+                model.Name,
+                model.Description,
+                model.StartDate,
+                model.EndDate,
+                model.TrainerId);
+
+            TempData.AddSuccessMessage($"Course {model.Name} created successful!");
+
+            return RedirectToAction(nameof(HomeController.Index), "Home", new {area = string.Empty});
+        }
+
+        private async Task<IEnumerable<SelectListItem>> GetTrainers()
+        {
+            var trainers = await this
+                .userManager
+                .GetUsersInRoleAsync(WebConstants.TrainerRole);
+            var trainerListItem = trainers
+                .Select(t => new SelectListItem
+                {
+                    Text = t.UserName,
+                    Value = t.Id
+                })
+                .ToList();
+
+            return trainerListItem;
+        }       
     }
 }
